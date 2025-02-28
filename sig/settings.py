fae15874221f439f26a6f4d4faef9e80a065a73d
codebadolo <codewithbadolo@gmail.com>
2025-02-28 20:25:27 +0000

    debug True

--- sig/settings.py
+++ sig/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = PROD_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Allowed hosts
 ALLOWED_HOSTS = ['sics-stage-cl.nasaraperilburkina.org', '127.0.0.1' ,'0.0.0.0']
