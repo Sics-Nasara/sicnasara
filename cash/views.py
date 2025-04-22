58332180e97c853e6faf56b4b9a2677e8511d0b9
codebadolo <codewithbadolo@gmail.com>
2025-04-22 16:09:27 +0000

    weasy in cash

--- cash/views.py
+++ cash/views.py
@@ -11,7 +11,7 @@ from django.db.models import Q, Sum, Prefetch, Count  , F
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.http import JsonResponse, HttpResponseRedirect
-from weasyprint import HTML
+#from weasyprint import HTML
 from django.forms import modelformset_factory
 from django.http import HttpResponse
 from django.template.loader import render_to_string
