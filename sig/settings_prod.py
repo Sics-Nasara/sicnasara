df96efd8f13b95a1f8b0b32f77ad7a80a4166a62
Badolo Geofroy <122725139+codebadolo@users.noreply.github.com>
2025-08-18 11:15:37 +0000

    Update settings_prod.py

--- sig/settings_prod.py
+++ sig/settings_prod.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = PROD_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SITE_NAME = 'SICS NASARA PROD'
 
